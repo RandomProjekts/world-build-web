2f01828685bde67a4744083282fd97880ef7099f
Noyavy <githubuser1@web.de>
2021-10-26 09:27:34 +0200

    added config and code to access a Database

---
+++ species.php
@@ -0,0 +1,13 @@
+ <?php
+$config = require (../config.php);
+// Create connection
+$conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['DBname']);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$conn->close();
+?> 
