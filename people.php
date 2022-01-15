2d62dd1251a0b96b982520ef7a386f3cc2528ece
sudo-Javy <githubuser1@web.de>
2022-01-16 00:25:56 +0100

    table added

--- people.php
+++ people.php
@@ -27,12 +27,48 @@ $query = $conn->prepare("SELECT * FROM people where id=?");
 $query->bind_param('i', $id);
 $query->execute();
 $result = $query->get_result()->fetch_assoc();
-echo ($result['name']);
 
-if (file_exists("../img/" . $result['name'] . ".JPG")) {
-    echo ("<img src=" . "../img/" . $result['name'] . ".JPG" . "><img/>");
-}
+
+
 ?> 
+<table>
+	<tr>
+		<th>Name</th>
+		<th><?php echo ($result['name']); ?></th>
+	</tr>
+	
+		<?php 
+		if (file_exists("../img/" . $result['name'] . ".JPG")) {
+		    echo ("
+                <tr>
+                    <th>Appearance</th>
+                    <th><img src=" . "../img/" . $result['name'] . ".JPG" . "></img></th>
+                </tr>
+                ");
+		}
+		?>
+	<tr>
+		<th>Gender</th>
+		<th><?php 
+		if ($result['gender'] == 'f') {
+		  echo("Female");
+		} elseif ($result['gender'] == 'm') {
+		  echo("Male");
+		}
+		
+		?></th>
+	</tr>
+	<tr>
+		<?php
+		if (!empty($result['age'])) {
+		    echo("
+                <th>Age</th>
+                <th>" . $result['age'] . " Years</th>"
+		        );
+		  }
+		?>
+	</tr>
+</table>
 
 <?php 
 $conn->close();
