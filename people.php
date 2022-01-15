baa477fc345380136dfc0ec8cad52a0ba58b93e7
sudo-Javy <githubuser1@web.de>
2022-01-15 01:06:51 +0100

    show name and img

--- people.php
+++ people.php
@@ -6,6 +6,13 @@
 <body>
 <?php
 $config = require ("../config.php");
+
+if (isset($_GET['id'])) {
+   $id = $_GET['id'];
+} else {
+   echo("no person selected");
+   exit();
+}
 // Create connection
 $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['DBname']);
 
@@ -15,7 +22,20 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-$conn->close();
+// Query
+$query = $conn->prepare("SELECT * FROM people where id=?");
+$query->bind_param('i', $id);
+$query->execute();
+$result = $query->get_result()->fetch_assoc();
+echo ($result['name']);
+
+if (file_exists("../img/" . $result['name'] . ".JPG")) {
+    echo ("<img src=" . "../img/" . $result['name'] . ".JPG" . "><img/>");
+}
 ?> 
+
+<?php 
+$conn->close();
+?>
 </body>
 </html>
