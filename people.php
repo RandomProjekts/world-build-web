f41741c41d0cea28289027b5c8ef8e351a9b3bcf
TenkouNoKitsune <66792136+TenkouNoKitsune@users.noreply.github.com>
2022-01-30 15:15:04 +0100

    automatic themecolor determination
    - themecolor now is determined automatically, though it can be manually overridden
    - spacing was automatically adjusted

--- people.php
+++ people.php
@@ -1,110 +1,120 @@
 <html>
-	<head>
+
+<head>
 	<?php
-	$conn = require ("./connection.php");
-	if (isset ( $_GET ['id'] )) {
-		$id = $_GET ['id'];
+	$conn = require("./connection.php");
+	if (isset($_GET['id'])) {
+		$id = $_GET['id'];
 	} else {
 		echo ("no person selected");
 		echo ("<title>People</title>");
-		exit ();
+		exit();
 	}
-	
+
 	// Query
-	$query = $conn->prepare ( "SELECT * FROM people where id=?" );
-	$query->bind_param ( 'i', $id );
-	$query->execute ();
-	$result = $query->get_result ()->fetch_assoc ();
-	
-	if (empty ( $result )) {
+	$query = $conn->prepare("SELECT * FROM people where id=?");
+	$query->bind_param('i', $id);
+	$query->execute();
+	$result = $query->get_result()->fetch_assoc();
+
+	if (empty($result)) {
 		echo ("Does not exist");
 		echo ("<title>People</title>");
-		exit ();
+		exit();
 	}
-	
-	?> 	
-		<title><?php echo ($result['name']); ?> - People</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/people.css">
-	</head>
-	<body>
-		
-		<table role="main">
-			<caption>
-				<h1><?php echo ($result['name']); ?></h1>
-			</caption>
-			
-			<?php
-			if (file_exists ( "../img/" . $result ['name'] . ".JPG" )) {
-				$b64img = base64_encode ( file_get_contents ( "../img/" . $result ['name'] . ".JPG" ) );
-				echo ("
+
+	?>
+	<title><?php echo ($result['name']); ?> - People</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="./css/people.css">
+	<link rel="stylesheet" href="./css/peopleSmall.css" media="all and (max-aspect-ratio: 3/5)">
+</head>
+
+<body>
+
+	<table role="main">
+		<caption>
+			<h1><?php echo ($result['name']); ?></h1>
+		</caption>
+
+		<?php
+		$imgpath = "../img/" . $result['name'] . ".JPG";
+		if (file_exists($imgpath)) {
+			$b64img = base64_encode(file_get_contents($imgpath));
+			echo ("
 		                <tr>
 		                    <th>Appearance</th>
 		                    <td><img src='data:image/png;base64,$b64img'></img></td>
 		                </tr>
 		                ");
+		}
+		?>
+		<tr>
+			<th>Gender</th>
+			<td><?php
+				if ($result['gender'] == 'f') {
+					echo ("Female");
+				} elseif ($result['gender'] == 'm') {
+					echo ("Male");
+				}
+
+				?>
+			</td>
+		</tr>
+		<tr>
+			<?php
+			if (!empty($result['age'])) {
+				echo ("
+		                <th>Age</th>
+		                <td>" . $result['age'] . " Years</td>");
 			}
 			?>
-			<tr>
-				<th>Gender</th>
-				<td><?php
-					if ($result ['gender'] == 'f') {
-						echo ("Female");
-					} elseif ($result ['gender'] == 'm') {
-						echo ("Male");
-					}
-		
-					?>
-				</td>
-			</tr>
-			<tr>
-				<?php
-				if (! empty ( $result ['age'] )) {
-					echo ("
-		                <th>Age</th>
-		                <td>" . $result ['age'] . " Years</td>");
-				}
-				?>
-			</tr>
-		</table>
-	</body>
+		</tr>
+	</table>
+</body>
 
-	<?php
-	/*
-	 * (optional) Main color of color sheme (appearance image) and version with alpha = 0
-	 * variables are used for coloring the rows in table
-	 * themecolor has to be a hex color string starting with a '#'
-	 */
-	if (! empty ( $result ['themecolor'] )) {
-		echo ("
-		<style>
-			:root {
-				--rowcolor: " . $result ['themecolor'] . ";
-				--rowcolor-transparent: " . $result ['themecolor'] . "00;
-			}
-		</style>
-		");
+<?php
+// Main color of color sheme (appearance image) and version with alpha = 0
+if (file_exists($imgpath)) {
+	include_once("./scripts/themecolor.php");
+	if (empty($result['themecolor'])) {
+		$themecolor = findthemecolor("../" . $imgfile); // imgpath adjusted for colorextract script
+	} else {
+		$themecolor = adjustlightness($result['themecolor']);
 	}
-	
-	$conn->close ();
-	?>
 
-	<script>
-		// hide content of rows that are "higher" than 30% of the viewport width and show only preview
-		var data = Array.from(document.getElementsByTagName("td"));
-		data.forEach(function(td) {
-			let h = td.offsetHeight;
-			if (h / window.innerWidth > 0.3) {
-				td.classList.add("shorten");
-				// toggle full content or preview on click
-				td.addEventListener("click", (e) => {
-					var v = e.target
-					while (v.tagName != "TD") {
-						v = v.parentNode;
-					}
-					v.classList.toggle("shorten");
-				});
-			}
-		});
-	</script>
+	if (!empty($themecolor)) {
+		echo ("
+	<style>
+		:root {
+			--rowcolor: " . $themecolor . ";
+			--rowcolor-transparent: " . $themecolor . "00;
+		}
+	</style>
+	");
+	}
+}
+
+$conn->close();
+?>
+
+<script>
+	// hide content of rows that are "higher" than 30% of the viewport width and show only preview
+	var data = Array.from(document.getElementsByTagName("td"));
+	data.forEach(function(td) {
+		let h = td.offsetHeight;
+		if (h / window.innerWidth > 0.3) {
+			td.classList.add("shorten");
+			// toggle full content or preview on click
+			td.addEventListener("click", (e) => {
+				var v = e.target
+				while (v.tagName != "TD") {
+					v = v.parentNode;
+				}
+				v.classList.toggle("shorten");
+			});
+		}
+	});
+</script>
+
 </html>
---
+++ scripts/themecolor.php
@@ -0,0 +1,64 @@
+<?php
+include_once("./colorextract.php");
+
+// used for ligtening up color
+function luminance($hexcolor, $percent)
+{
+    if (strlen($hexcolor) < 6) {
+        $hexcolor = $hexcolor[0] . $hexcolor[0] . $hexcolor[1] . $hexcolor[1] . $hexcolor[2] . $hexcolor[2];
+    }
+    $hexcolor = array_map('hexdec', str_split(str_pad(str_replace('#', '', $hexcolor), 6, '0'), 2));
+
+    foreach ($hexcolor as $i => $color) {
+        $from = $percent < 0 ? 0 : $color;
+        $to = $percent < 0 ? $color : 255;
+        $pvalue = ceil(($to - $from) * $percent);
+        $hexcolor[$i] = str_pad(dechex($color + $pvalue), 2, '0', STR_PAD_LEFT);
+    }
+
+    return '#' . implode($hexcolor);
+}
+
+function findthemecolor($imgfile)
+{
+    $ex = new GetMostCommonColors();
+    $colors = $ex->Get_Color($imgfile, 4, true, true, 24);
+    // find most colorful color
+    $best = -1;
+    foreach (array_keys($colors) as $c) {
+        $crgb = array(hexdec(substr($c, 0, 1)), hexdec(substr($c, 2, 1)), hexdec(substr($c, 4, 1)));
+        $crp = $crgb[0] / 255;
+        $cgp = $crgb[1] / 255;
+        $cbp = $crgb[2] / 255;
+        # saturation of color
+        if (max($crp, $cgp, $cbp)) {
+            $S = (max($crp, $cgp, $cbp) - min($crp, $cgp, $cbp)) / max($crp, $cgp, $cbp);
+        } else {
+            $S = 0;
+        }
+        $cL = (max($crp, $cgp, $cbp) - min($crp, $cgp, $cbp)) / 2; // lightness of color
+        if ($S * $cL > $best) {
+            $best = $S * $cL;
+            $rgb = $crgb;
+            $hex = $c;
+            $rp = $crp;
+            $gp = $cgp;
+            $bp = $cbp;
+            $L = $cL;
+        }
+    }
+    // if color is too dark, lighten it up, if too light, darken
+    $themecolor = luminance($hex, 0.8 - $L);
+    return $themecolor;
+}
+
+function adjustlightness($hex)
+{
+    $rgb = array(hexdec(substr($hex, 0, 1)), hexdec(substr($hex, 2, 1)), hexdec(substr($hex, 4, 1)));
+    $rp = $rgb[0] / 255;
+    $gp = $rgb[1] / 255;
+    $bp = $rgb[2] / 255;
+    $L = (max($rp, $gp, $bp) - min($rp, $gp, $bp)) / 2; // lightness of color
+    $themecolor = luminance($hex, 0.8 - $L);
+    return $themecolor;
+}
