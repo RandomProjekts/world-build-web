64891b4a7e23a0fdb0dd7f2c6c56597bd4300d06
NoYavy <githubuser1@web.de>
2022-04-11 00:05:56 +0200

    fixed quotation marks in config.php
    Co-authored-by: Ein Typ <34921967+eintyp@users.noreply.github.com>

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 // this goes one directory higher than the other files
 // just change this to your own credentials
 return array(
-'servername' => "localhost",
-'username' => "username",
-'password' => "password",
-'DBname' => "worldDB",
+'servername' => 'localhost',
+'username' => 'username',
+'password' => 'password',
+'DBname' => 'worldDB',
 );
 ?>
