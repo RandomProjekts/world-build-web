e0531dade1683c640e548fa0a1b8c5f74d22e845
NoYavy <githubuser1@web.de>
2022-04-11 00:32:51 +0200

    fixed closing tags
     to match php convention

--- config.php
+++ config.php
@@ -8,4 +8,3 @@ return array(
 'password' => 'password',
 'DBname' => 'worldDB',
 );
-?>
--- scripts/connection.php
+++ scripts/connection.php
@@ -8,4 +8,3 @@ if ($conn->connect_error) {
 	die ( "Connection failed: " . $conn->connect_error );
 }
 return $conn;
-?>
