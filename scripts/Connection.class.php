9c79e4aae9bc0d02c71740fef5fd4aa23a4201fd
eintyp <eintyp@duck.com>
2022-04-14 11:26:22 +0200

    #5 properly access static property, thanks to php 7 for not throwing any errors

--- scripts/Connection.class.php
+++ scripts/Connection.class.php
@@ -8,20 +8,20 @@ class Connection {
 	protected static $conn;
 
 	public function __construct() {
-		if (!isset($this->conn)) {
+		if (!isset(self::$conn)) {
 			$config = require ($_SERVER['DOCUMENT_ROOT'] . '/../config.php');
 			// Create connection
-			$this->conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['DBname']);
+			self::$conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['DBname']);
 
 			// Check connection
-			if ($this->conn->connect_error) {
-				die('Connection failed: ' . $this->conn->connect_error);
+			if (self::$conn->connect_error) {
+				die('Connection failed: ' . self::$conn->connect_error);
 			}
 		}
 	}
 
 	public function getConection() {
-		return $this->conn;
+		return self::$conn;
 	}
 
 }
